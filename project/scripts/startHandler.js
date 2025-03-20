6b19544dbb6fd60134ece3cb9ae82240c83e2593
Adnan Muzaferovic <a.muzaferovic@students.htl-leonding.ac.at>
2025-03-20 14:01:25 +0100

    erste animationen & fullscreen code snippet von w2schools

--- project/scripts/startHandler.js
+++ project/scripts/startHandler.js
@@ -1,5 +1,8 @@
 let indexBody = document.getElementById('startBody');
 
+// on Page open
+startSequence();
+
 // Intro
 
 function startSequence() {
@@ -11,7 +14,44 @@ function startSequence() {
     <h2>It is recommended to use headphones and fullscreen (F11) for the best experience.</h2>
     </div>
     `
+    let introTextBox = document.getElementById('introTextBox');
+    setTimeout(function() {
+        introTextBox.style.animation = 'none';
+        introTextBox.style.animation = 'fadeIn 0.5s 0s ease-in-out';
+        introTextBox.style.opacity = 1;
+    }, 500)
+    setTimeout(function() {
+        introTextBox.style.animation = 'none';
+        introTextBox.style.animation = 'fadeOut 0.5s 0s ease-in-out';
+        introTextBox.style.opacity = 0;
+    }, 4000)
 }
 
-// on Page open
-startSequence();
+// ------------------------------------------------------------------------------------------------------
+// FULLSCREEN CODE SNIPPET FROM W2SCHOOLS | Link: https://www.w3schools.com/howto/howto_js_fullscreen.asp
+// ------------------------------------------------------------------------------------------------------
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
