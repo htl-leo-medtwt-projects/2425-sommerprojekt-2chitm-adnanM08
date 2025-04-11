a99e9a3899dfb50eb1331f183b21f5e1a3871157
Adnan Muzaferovic <a.muzaferovic@students.htl-leonding.ac.at>
2025-04-11 12:03:04 +0200

    playerHandler js datei hinzugefügt

---
+++ project/scripts/playerHandler.js
@@ -0,0 +1,6 @@
+// Player configuration
+let PLAYER = {
+    'lifeStatus': 'alive', // Options: alive | dead
+    'level': 1, // Level (night) the player is currently in. Options: 1 | 2 | 3 | 4 | 5
+    'stars': 0, // How many stars the player has. Stars can be obtained by winning all Levels (nights)
+}
